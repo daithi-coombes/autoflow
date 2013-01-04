72c1014480970562292fc549e365801fb545c381
daithi <daithi@daithi-laptop>
2013-01-04 17:14:28 +0000

    initial commit

---
+++ application/Controller.class.php
@@ -0,0 +1,368 @@
+<?php
+
+if(!class_exists("WPPluginFrameWorkController")):
+	/**
+	 * Plugin modules that want to make use of the framework methods should
+	 * extend this class.
+	 * 
+	 * The purpose of this framework is allow the easy creation of plugins for
+	 * wordpress by simply dropping in a class and modulising your project. The 
+	 * index.php file acts as a bootstrap using the
+	 * @see WPPluginFrameWorkConfig class
+	 * 
+	 * By modulising your plugin, then creating a module class file and
+	 * extending @see WPPluginFrameWorkConfig class it will be easier to access 
+	 * front and admin functionality. Using a nameing scheme and giving view 
+	 * files, javascript and css files the same name they are automatically 
+	 * hooked up to your class.
+	 * 
+	 * By keeping the framework as a wrapper around your plugin this allows your
+	 * code to be in full control only use what you need from the framework.
+	 * 
+	 * -----------
+	 * JavaScripts
+	 * -----------
+	 * The class will automatically try to load:
+	 * public_html/js/${module}.js
+	 * by default. If this is not there then it will try to load:
+	 * public_html/js/${module}.min.js
+	 * 
+	 * 3rd party scrips are registered using the config class in the index.php
+	 * file to allow registering of 3rd party scripts plugin wide. To register a
+	 * 3rd party script use:
+	 * $config->third_party['scripts'] = array( handle=>src )
+	 * 
+	 * Dependencies can be loaded by the child class in the __construct like so:
+	 * $this->scripts = array( 'jquery' );
+	 * 
+	 * @see WPPluginFrameWorkController::load_scripts()
+	 * @see WPPluginFrameWorkConfig::register_3rd_parties()
+	 * ----------------------------------------------------
+	 * 
+	 * @author daithi
+	 * @package cityindex
+	 * @subpackage ci-wp-login
+	*/
+	class WPPluginFrameWorkController{
+
+		/** @var string Holds actions from $_REQUEST[$action_key] for calling method
+		* in child class.
+		*/
+		public $action = false;
+		/** @var array The configuration struct defined in plugin index.php file */
+		public $config = array();
+		/** @var array Array of js dependencies called in child class */
+		public $script_deps = array();
+		/** @var array Associative array of shortcode=>value pairs */
+		public $shortcodes = array();
+		/** @var array Array of css dependencies called in child class */
+		public $style_deps = array();
+		/** @var array An associative array of wordpress actions and callbacks */
+		public $wp_action = array();
+		/** @var array An associative array of wordpress filters and callbacks */
+		public $wp_filter = array();
+		/** @var string lowercase child class name */
+		private $class = false;
+		/** @var string Camel case child class name */
+		private $class_name = false;
+		/** @var string The html code for the view file */
+		private $html = false;
+
+		/**
+		* Construct controller.
+		*
+		* @param string $class The class name being extended
+		* @param string $plugin_dir The plugin dir
+		* @param string $plugin_url The plugin url
+		*/
+		function __construct( $class=false ){
+			
+			if($class==false) return;
+
+			//set default fields
+			global $config;
+
+			$this->class = $class;
+			$this->class_name = ucfirst( @array_pop(explode("\\", $class)) );
+			$this->config = $config;
+			(@$_REQUEST[$this->config->action_key])
+				? $this->action = $_REQUEST[$this->config->action_key]
+				: $this->action = false;
+
+			//default methods
+			$this->do_action();
+			$this->set_wp_actions();
+			$this->set_wp_filters();
+		}
+
+		/**
+		* Activation callback.
+		* 
+		* Installs tables 
+		*/
+		public function activate(){
+
+			require_once( ABSPATH . '/wp-admin/includes/upgrade.php');
+
+			/**
+			*CREATE TABLE IF NOT EXISTS `wp_CityIndex\WP\Login_WP_Groups_Group` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name` varchar(20) COLLATE utf8_bin NOT NULL,
+	`description` text COLLATE utf8_bin NOT NULL,
+	PRIMARY KEY (`id`)
+	) 
+			*/
+
+			foreach($this->modal_tables as $table=>$fields){
+				$sql = "
+					CREATE TABLE IF NOT EXISTS `{$this->modal_prefix}_{$table}`("
+					. implode(",", $fields)
+					. ");";
+				dbDelta($sql);
+			}
+		}
+
+		/**
+		* Creates nonces for the view file.
+		* 
+		* @see Controller::set_shortcodes()
+		* @return string
+		*/
+		public function create_nonce( $arr ){
+			return wp_create_nonce($arr[0]);
+		}
+
+		/**
+		* Log an error.
+		* 
+		* @param string $err The error message.
+		* @return boolean false Method always returns false.
+		*/
+		public function error( $err ){
+			$this->config->errors[] = $err;
+			return false;
+		}
+
+		/**
+		* Checks $_REQUEST[_wpnonce] against the param $action.
+		* 
+		* Will die() if verification fails.
+		*  
+		* @see wp_verify_nonce()
+		* @param string $action The action word used setting up the nonce.
+		* @param boolean $json Default True. Whether to return json error or plain
+		* text.
+		*/
+		public function check_nonce($action, $json=false){
+			if(!wp_verify_nonce($_REQUEST['_wpnonce'], $action)){
+				if($json){
+					print json_encode(array('error' => 'invalid nonce'));
+					die();
+				}
+				else{
+					$this->error("Invalid Nonce");
+					return false;
+				}
+			}
+			return true;
+		}
+
+		/**
+		* Looks for an action method to call in child class. 
+		*/
+		public function do_action(){
+			$action = $this->action;
+			if(method_exists($this, $action))
+				add_action('init', array($this, $action));
+		}
+
+		/**
+		* Prints the view html.
+		* 
+		* Loads the html then sets shortcodes,loads scripts and styles then prints 
+		* html.
+		* 
+		* @return void
+		*/
+		public function get_page() {
+			
+			//vars
+			$this->html = file_get_contents("{$this->config->plugin_dir}/public_html/{$this->class_name}.php");
+
+			//clean out phpDoc
+			$this->html = preg_replace("/<\?php.+\?>/msU", "", $this->html);
+
+			$this->shortcodes['errors'] = $this->get_errors();
+			$this->shortcodes['messages'] = $this->get_messages();
+
+			$this->set_shortcodes();			
+			$this->load_scripts();
+			$this->load_styles();
+
+			print $this->html;
+		}
+
+		/**
+		 * Add message to config messages array.
+		 * 
+		 * @param string $msg The message to add.
+		 * @return true Always returns true. 
+		 */
+		public function message($msg){
+			$this->config->messages[] = $msg;
+			return true;
+		}
+		
+		/**
+		* Return html list of errors reported by plugin globally.
+		* 
+		* @return string Returns an html <ul> list of errors.
+		*/
+		private function get_errors(){
+			
+			$html = "<div id=\"message\" class=\"error\"><ul>\n";
+			$errors = $this->config->errors;
+			
+			if(@$_REQUEST['error'])
+				$errors[] = $_REQUEST['error'];
+
+			if(!count($errors)) return false;
+			foreach($errors as $err)
+				$html .= stripslashes ("<li>{$err}</li>\n");
+
+			wp_enqueue_style('colors');
+			return $html .= "</ul>\n</div>\n";
+		}
+
+		/**
+		 * Return html list of messages reported by plugin globally.
+		 * 
+		 * @return string Returns an html ul list of messages. 
+		 */
+		private function get_messages(){
+
+			$html = "<div id=\"message\" class=\"message updated\"><ul>\n";
+			$messages = $this->config->messages;
+
+			if(@$_REQUEST['message'])
+				$messages[] = $_REQUEST['message'];
+
+			if(!count($messages)) return false;
+			foreach($messages as $msg)
+				$html .= stripslashes ("<li>{$msg}</li>\n");
+
+			wp_enqueue_style('colors');
+			return $html .= "</ul>\n</div>\n";
+		}
+
+		/**
+		* Registers action calls with wp core.
+		* 
+		* Loops through $this->action_wp array, takes the key as the action and the
+		* value as the callback.
+		* 
+		* @return void
+		*/
+		private function set_wp_actions(){
+
+			if(!count($this->wp_action)) return;
+			foreach($this->wp_action as $action=>$call){
+				add_action($action, $call);
+			}
+		}
+
+		/**
+		* Registers action calls with wp core.
+		* 
+		* Loops through $this->action_wp array, takes the key as the action and the
+		* value as the callback.
+		* 
+		* @return void
+		*/
+		private function set_wp_filters(){
+
+			if(!count($this->wp_filter)) return;
+			foreach($this->wp_filter as $action=>$call){
+				add_action($action, $call);
+			}
+		}
+
+		/**
+		 * Loads javascript files.
+		 * 
+		 * Will look for {$module}.js in public/js folder by default. If not
+		 * found will look for the {$module}.min.js in the same folder.
+		 * 
+		 * @return void 
+		*/
+		private function load_scripts() {
+			
+			//vars
+			$js_orig = "public_html/js/{$this->class_name}.js";
+			$js_min = "public_html/js/{$this->class_name}.min.js";
+			$js = false;
+			
+			//check if js file found (priority to $js_orig)
+			if(@file_exists("{$this->config->plugin_dir}/{$js_orig}")) 
+				$js=$js_orig;
+			elseif(@file_exists("{$this->config->plugin_dir}/{$js_min}"))
+				$js=$js_min;
+			
+			//if js file found
+			if($js){
+				wp_register_script($this->class_name, "{$this->config->plugin_url}/{$js}", $this->script_deps);
+				wp_enqueue_script( $this->class_name );
+			}
+		}
+
+		/**
+		* Loads css files
+		* 
+		* @return void 
+		*/
+		public function load_styles() {
+
+			//include dependencies
+			foreach($this->style_deps as $dep)
+				wp_enqueue_style ( $dep );
+
+			//look for class specific style
+			if(!file_exists("{$this->config->plugin_dir}/public_html/css/{$this->class_name}.css")) return;
+
+			wp_register_style($this->class_name, "{$this->config->plugin_url}/public_html/css/{$this->class_name}.css", $this->style_deps);
+			wp_enqueue_style( $this->class_name );
+		}
+
+		/**
+		* Sets values for the shortcodes in the view file.
+		* 
+		* Replaces the codes with values in $this->html. To add shortcodes to the 
+		* view file use the syntax:
+		* <code> <!--[--identifying string--]--> </code>. 
+		* In $this->get_page() add the value to the array $this->shortcodes[].
+		* eg: $this->shortcodes['identifying string'] = "value".
+		* 
+		* @return void
+		*/
+		private function set_shortcodes() {
+
+			$this->shortcodes['errors'] = $this->get_errors();
+			$this->shortcodes['messages'] = $this->get_messages();
+			
+			//replace values
+			if(count(@$this->shortcodes))
+				foreach ($this->shortcodes as $code => $val){
+
+					//if val is method call
+					if(is_array($val)){
+						$method = array_shift($val);
+						if(method_exists($this, $method))
+							$val = $this->$method( $val );
+						else $val = "unkown shortcode";
+					}
+					$this->html = str_replace("<!--[--{$code}--]-->", $val, $this->html);
+				}
+		}
+
+	}
+endif;
---
+++ application/modules/AutoFlow_API.class.php
@@ -0,0 +1,198 @@
+<?php
+
+//include api-connection-manager class
+require_once( WP_PLUGIN_DIR . "/api-connection-manager/class-api-connection-manager.php");
+
+class AutoFlow_API{
+
+	/** @var API_Connection_Manager The api connection manager */
+	private $api;
+
+	/**
+	 * Construct.
+	 * 
+	 * @global API_Connection_Manager $API_Connection_Manager 
+	 */
+	function __construct(){
+
+		//load api connection manager
+		global $API_Connection_Manager;
+		if(!$API_Connection_Manager)
+			$API_Connection_Manager = new API_Connection_Manager();
+		$this->api = $API_Connection_Manager;
+		
+		/**
+		 * add login form hook
+		 * login form error messages hook
+		 * add shortcode for custom theme forms
+		 */
+		add_action( 'login_footer', array( &$this, 'print_login_buttons' ) );
+		//add_filter( 'login_message', array(&$this, 'print_login_errors' ) );
+		add_shortcode( 'AutoFlow', array( &$this, 'print_login_buttons' ) );		
+	}
+
+	/**
+	 * Prints the login buttons.
+	 * 
+	 * Callback for login_footer hook
+	 */
+	public function print_login_buttons(){
+		
+		//vars
+		$services = $this->api->get_services();
+		$res = "<ul>\n";
+		
+		//build list of buttons
+		foreach($services as $slug => $service)
+			$res .= "<li>" . $this->api->get_login_button( $slug, __FILE__, array(&$this, 'parse_dto') );
+		
+		//print result
+		print "{$res}\n</ul>\n";
+	}
+	
+	/**
+	 * Callback that will redirect user.
+	 * 
+	 * The dto passed by API_Connection_Manager will be in the format:
+	 * $dto stdClass
+	 *     ->response (array)
+	 *         [code] (string)
+	 *         [state] (string)
+	 *     ->slug (string)
+	 *     ->user (integer|NULL)
+	 *     ->token (string)
+	 * 
+	 * @param stdClass $dto The response dto.
+	 */
+	public function parse_dto( stdClass $dto ){
+		
+		//make request for email
+		switch ($dto->slug) {
+			
+			/**
+			 * Github 
+			 */
+			case 'github/index.php':
+
+				$res = $this->api->request( $dto->slug, array(
+					'uri' => "https://api.github.com/user/emails?access_token={$dto->access_token}&scope=user,public_repo",
+					'headers' => array(
+						'Accept' => 'application/json'
+					),
+					'method' => 'get',
+					'access_token' => $dto->access_token
+				));
+				
+				$emails = (array) json_decode($res['body']);
+				break;
+			//end Github
+
+			/**
+			 * Google 
+			 */
+			case 'google/index.php':
+				
+				$res = $this->api->request($dto->slug, array(
+					'uri' => "https://www.googleapis.com/oauth2/v1/userinfo?access_token={$dto->access_token}", 
+					'headers' => array(
+						'Authorization' => "Bearer {$dto->access_token}"
+					),
+					'method' => 'get',
+					'access_token' => $dto->access_token
+				));
+				
+				$emails = array( json_decode($res['body'])->email );
+				break;
+			//end Google
+			
+			/**
+			 * Facebook 
+			 */
+			case 'facebook/index.php':
+				
+				$res = $this->api->request($dto->slug, array(
+					'uri' => "https://graph.facebook.com/me?access_token={$dto->access_token}",
+					'method' => 'get',
+					'access_token' => $dto->access_token
+				));
+				
+				$emails = (array) json_decode($res['body'])->email;
+				break;
+			//end Facebook
+			
+			/**
+			 * Default die( error ) 
+			 */
+			default:
+				die("<b>AutoFlow API</b>: Unkown service <em>{$dto->slug}</em><br/>
+					Please add code to get account emails in <em>AutoFlow_API::parse_dto()</em>");
+				break;
+		}//end switch slug
+		
+		//get user object by email
+		foreach ( (array) $emails as $email ) {
+			$user_id = email_exists( $email );
+			if ( $user_id ){
+				$user = get_userdata( $user_id );
+				break;
+			}
+		}
+
+		//error report
+		if(!$user)
+			die("<b>AutoFlow API</b>:<br/>
+				Email on service account <em>{$email}</em> does not match any on profiles on this blog");
+		
+		//log in user
+		wp_set_current_user( $user->data->ID );
+		wp_set_auth_cookie( $user->data->ID );
+		$this->api->set_user_token($dto->slug, $dto->access_token, 'access', $user->data->ID);
+		if(@$dto->refresh_token)
+			$this->api->set_user_token($dto->slug, $dto->refresh_token, 'refresh', $user->data->ID);
+		wp_redirect("/wp-admin");
+		exit();
+	}
+	
+	/**
+	 * Returns the emails for an account on a service.
+	 * 
+	 * Is used for logging and testing access_tokens.
+	 * 
+	 * @todo change so always returns an array instead of string|array
+	 * @todo use oauth2's token checking calls instead
+	 * @param string $slug The service slug
+	 * @param string $access_token The access token
+	 * @return emails 
+	 * @deprecated
+	 * @subpackage service-method
+	 */
+	private function _service_get_account_emails($slug, $access_token){
+		
+		$service = $this->get_service($slug);
+		$params = $service['params'];
+		$req = array();
+		$req['access_token'] = $access_token;
+		$emails = false;
+		$user = false;
+
+		//get request for account info
+		if("get"==strtolower($params['account-method'])){
+			$url = url_query_append($params['account-uri'], $req);
+			$res = wp_remote_get($url);
+		}
+		
+		//look for email from result
+		if('json'==strtolower($params['account-response-type'])){
+			$res = json_decode($res['body']);
+			
+			$key = $params['account-response-email-var'];
+			if(@$res->$key)
+				$emails = $res->$key;
+			if(is_array($res))
+				$emails = $res;
+		}
+		
+		if(!$emails) return $this->_error("No emails returned");
+		return $emails;
+	}	
+}
