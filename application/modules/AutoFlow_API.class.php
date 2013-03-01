a7a6db4ce80b056d535098d4538dd2ae3658ac77
daithi <daithi@daithi-laptop>
2013-03-01 19:01:31 +0000

    no special chars in username https://github.com/cityindex/labs.cityindex.com/issues/98

--- application/modules/AutoFlow_API.class.php
+++ application/modules/AutoFlow_API.class.php
@@ -61,7 +61,7 @@ class AutoFlow_API extends WPPluginFrameWorkController{
 		//vars
 		global $API_Connection_Manager;
 		$view = new API_Con_Mngr_View();
-		$username = wp_generate_password(6);
+		$username = wp_generate_password(6, false);
 		$password = wp_generate_password( 12, false );
 		while(username_exists($username))	//make sure username is unique 
 			$username = wp_generate_password(6);
